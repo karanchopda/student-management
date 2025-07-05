a83a56f3bbaf5b9ef2663a75aae08ca30989190f
karanchopda <your_email@example.com>
2025-07-05 16:55:14 +0530

    fix: use MongoDB Atlas in Render

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/studentdb', {
+mongoose.connect(process.env.MONGODB_URI ,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
