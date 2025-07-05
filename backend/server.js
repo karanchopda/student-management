c409c1b2a8f29d6162e5d570a1e69ccd08cb75dd
karanchopda <your_email@example.com>
2025-07-05 17:45:16 +0530

    Fix: CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://student-management-eosin-sigma.vercel.app/'], 
+  credentials: true
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI , {
