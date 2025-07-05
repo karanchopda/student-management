779f5a4387c3a9a549230cab01ccea807f5e6517
karanchopda <your_email@example.com>
2025-07-05 17:20:24 +0530

    fix: correct env variable key for MongoDB URI

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI ,{
+mongoose.connect(process.env.MONGO_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
