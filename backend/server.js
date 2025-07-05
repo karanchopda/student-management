44e864a47e4b79cf27e2178fa17b8e0ba0f79cc4
karanchopda <your_email@example.com>
2025-07-05 18:28:11 +0530

    fix: update CORS to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.use(cors({
-  origin: [
-    'student-management-eosin-sigma.vercel.app', 
-  ]
+  origin: '*',
 }));
 
 
